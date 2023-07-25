4f287f62b8950ee477759deef7dfa5a430af9d62
harshavictory <140200709+harshavictory@users.noreply.github.com>
2023-07-25 22:25:49 +0530

    Create main.cpp
    First commit 🥇 Created main.cpp file.

---
+++ main.cpp
@@ -0,0 +1,29 @@
+
+// Square root
+int f_3_1_1(int number)
+{
+    if(number < 2)
+        return number;
+  
+    int result = -1;
+    int g2 = number/2;
+    int g1 = g2;
+    
+    while((g2*g2) >= number)
+    {
+        g1 = g2;
+        g2 = (g1 + number/g1)/2;
+        if(g2*g2 == number)
+        {
+            result = g2;
+            break;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+  f_3_1_1(64);
+  return 0;
+}
