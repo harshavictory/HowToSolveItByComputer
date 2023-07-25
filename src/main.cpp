26474b37cf136f58a024eda169b75066055be5c2
Harshavardhan Suswaram <140200709+harshavictory@users.noreply.github.com>
2023-07-25 23:09:09 +0530

    Update main.cpp
    Added function to calculate the pass percentage of the given student marks. Reads the marks from text file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,33 @@
+#include <iostream>
+
+void f_2_2_1()
+{
+    float maxMarks=100.0;
+    unsigned int noOfPasses=0;
+    unsigned int totalMarks=0;
+    std::vector < std::string > marks;
+    std::ifstream file ("marks.txt");
+    if (file.is_open ())
+    {
+        while (!file.eof())
+	    {
+	        std::string value;
+	        std::getline(file, value);
+	        marks.push_back(value);
+	        auto intVal = std::stoi(value); //throws exception, need to handle.
+	        if(std::stoi(value) >= 50)
+	        {
+	            ++noOfPasses;
+	        }
+	        totalMarks+=intVal;
+	    }
+    }
+    
+    //auto totalMarks =  std::accumulate(marks.begin(), marks.end());
+    std::cout<<"Total marks: " << totalMarks<<std::endl;
+    std::cout<<"no of pass marks: " << noOfPasses <<std::endl;
+    std::cout<<"Percentage pass rate: " << std::floor(maxMarks/static_cast<float>(marks.size()) * static_cast<float>(noOfPasses)) << "%" << std::endl;
+}
 
 // Square root
 int f_3_1_1(int number)
